3a3e105d606aec83362e14b447d6df5132d2461b
webapps\Perumal <nandhanandhu1991@gmail.com>
2017-07-03 03:32:06 -0700

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 import { FacebookModule } from 'ngx-facebook';
 
 import { AppComponent } from './app.component';
+import { Page1Component } from './page1/page1.component';
+import { Page2Component } from './page2/page2.component';
+import { RouterModule, Routes} from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page1Component,
+    Page2Component
   ],
   imports: [
     BrowserModule,
-    FacebookModule.forRoot()
+    FacebookModule.forRoot(),
+    RouterModule.forRoot([
+    	{path: 'page1', component: Page1Component },{path: 'page2', component: Page2Component }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
