fc8d8242dd8a2dfdf8ec29382fc88a16cf8b49e2
webapps\Perumal <nandhanandhu1991@gmail.com>
2017-07-09 08:50:14 -0700

    routing params

--- src/app/page1/page1.component.ts
+++ src/app/page1/page1.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Routes, ActivatedRoute, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page1',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page1.component.css']
 })
 export class Page1Component implements OnInit {
+	id: string;
+	constructor(private route: ActivatedRoute) { 
+		route.paramMap.subscribe(params => this.id = params.get('id'));
+		 console.log(this.id);
+	}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
